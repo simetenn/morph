25c05bb3d866ae90e5def7a8834a8c9e995ee3e1
simetenn@gmail.com <simetenn@gmail.com@a0903317-16f6-7f7a-809d-c0b8deb45b1e>
2012-09-07 13:32:00 +0000

    Before changing the code

--- trunk/CParticle.h
+++ trunk/CParticle.h
@@ -4,48 +4,48 @@
 
 class CParticle : public CVector {
  public:
-  
-  CParticle();
-  CParticle(double* in_array);
-  ~CParticle();
-  
-    
-  void Set_Position(double Px, double Py, double Pz);
-  void Set_Velocity(double Vx, double Vy, double Vz);
-  void Set_Acceleration(double Ax, double Ay, double Az);
-  void Set_Mass(double in_M);
-  void Set_Charge(double in_C);
-  void Set_Data(vector<double> data);
 
-  double* Particle2Array();
+	CParticle();
+	CParticle(double* in_array);
+	~CParticle();
 
-  int get_ID();
-  double get_Mass();
-  CVector& get_P();
-  CVector& get_V();
-  CVector& get_A();
- 
 
-  double Ekin();
-  CVector Momentum();
-  void Move(double dt);
-  
-  
-  void print_Particle();
-  
+	void Set_Position(double Px, double Py, double Pz);
+	void Set_Velocity(double Vx, double Vy, double Vz);
+	void Set_Acceleration(double Ax, double Ay, double Az);
+	void Set_Mass(double in_M);
+	void Set_Charge(double in_C);
+	void Set_Data(vector<double> data);
 
-  CVector P,V,A;
-  
-  protected:
-  
-  
-  double Mass, Charge;
-  int ID;
-  
-  static int ID_Generator,ParticleSize;
+	double* Particle2Array();
 
-  void Generate_ID();
-    
+	int get_ID();
+	double get_Mass();
+	CVector& get_P();
+	CVector& get_V();
+	CVector& get_A();
+
+
+	double Ekin();
+	CVector Momentum();
+	void Move(double dt);
+
+
+	void print_Particle();
+
+
+	CVector P,V,A;
+
+ protected:
+
+
+	double Mass, Charge;
+	int ID;
+
+	static int ID_Generator,ParticleSize;
+
+	void Generate_ID();
+
 
 
 };
