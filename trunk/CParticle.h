8c3fb1e6b7dc055a29d5dc92962ca39d950c791c
simetenn@gmail.com <simetenn@gmail.com@a0903317-16f6-7f7a-809d-c0b8deb45b1e>
2012-10-09 13:33:16 +0000

    Cleaning up code

--- trunk/CParticle.h
+++ trunk/CParticle.h
@@ -5,64 +5,82 @@ class CParticle{
  public:
 
 	CParticle();
+	
+	//Create a particle from an array on the following form:
+	//[HaloID,Mass,Charge,Px,Py,Pz,Vx,Vy,Vz,Ax,Ay,Az]
 	CParticle(double* in_array);
+
 	~CParticle();
 
-	CParticle* next,*prev;
 
-	
-	
+	//Gives the next and previous particle in a linked list
+	CParticle* next,*prev;
+	//Removes the particle from a linked list
+	void RemoveFromList();
+
+
+	//Set the different values a particle has
+	void setHalo(int element);
+	void Set_Mass(double in_M);
+	void Set_Charge(double in_C);
 	void Set_Position(double Px, double Py, double Pz);
 	void Set_Velocity(double Vx, double Vy, double Vz);
 	void Set_Acceleration(double Ax, double Ay, double Az);
-	void Set_Mass(double in_M);
-	void Set_Charge(double in_C);
+
+	//Set all data, given by a vector
 	void Set_Data(vector<double> data);
-	
+
 	void setP(CVector inP);
 	void setV(CVector inV);
 
-	int getFlag();
-	void setFlag(int inFlag);
-	
-	double* Particle2Array();
 
+	//Get the different values a particle has
 	int get_ID();
+	int getHalo();
 	double get_Mass();
 	CVector& get_P();
 	CVector& get_V();
 	CVector& get_A();
-
-
-	double Ekin();
-	CVector Momentum();
-	void Move(double dt);
 	int getParticleSize();
 
+
+	//Get/set flag value.
+	//Used in FoF methods to tell if this particle has been tested yet or not
+	void setFlag(int inFlag);
+	int getFlag();
+
+
+	//Convert a particle to an array on the form:
+	//[HaloID,Mass,Charge,Px,Py,Pz,Vx,Vy,Vz,Ax,Ay,Az]
+	double* Particle2Array();
+
+	//Calculate the kinetic energy of a particle
+	double Ekin();
+	//Calculate the momentumof a particle
+	CVector Momentum();
+	//Move the particle one timestep
+	void Move(double dt);
+
+
+	//Print out all the information contained in one particle
 	void print();
 
-	void RemoveFromList();
-	
-	void setHalo(int element);
-	void decreaseHalo();
+
+
+	//Test if a particle has been assigned a halo
 	int gotHalo();
-	int getHalo();
-	
-	//put this private, and get the code to work in some way. ???
+	//Decreases the HaloID by one. Used by my inefficient FoF code
+	void decreaseHalo();
+
+
+	//put this private, and get the code to work in some way.
 	CVector P,V,A;
-	
+
  protected:
-
-	int flag;
-	double Mass, Charge;
-	int ID, HaloID;
-
-	int ParticleSize;
-	
 	static int ID_Generator;
 
+	int ID, HaloID,ParticleSize,flag;
+	double Mass, Charge;
+
 	void Generate_ID();
-
-
-
 };
--- trunk/CVector.h
+++ trunk/CVector.h
@@ -1,57 +1,87 @@
 #include <vector>
 using namespace std;
 
+
+
+//A simple CVector class only used for storing data into when
+//reading from a binary file. The data must then be transfered
+//into the complete CVector class
 class CVector_bare{
  public:
 	double x,y,z;
 };
 
+
+
 class CVector{
  public:
 
-  CVector();
-  CVector(double in_x, double in_y, double in_z);
-  CVector(vector<double> in_Vector);
-  CVector(const CVector& in_Vector);
+	CVector();
+	CVector(double in_x, double in_y, double in_z);
+	CVector(vector<double> in_Vector);
+	CVector(const CVector& in_Vector);
 
-  ~CVector();
+	~CVector();
 
-  void print();
-  double& operator[](int element);
-  CVector operator+(const CVector& other) const;
-  //CVector operator+(int number);
-  CVector Add(double number) const;
-  
-  CVector operator-(const CVector& other) const;
-  CVector Subtract(double number) const;
-  
-  CVector operator/(double number) const;
-  CVector operator*(double number) const;
-  
-  int getDimensions();
-  
-  double Length2() const;
-  double Length() const;
-  
-  CVector Normalize() const;
 
-  CVector Cross(const CVector& other) const;
-  double Dot(const CVector& other) const;
-  
-  double Sum() const;
-  
-  void Set(double x,double y,double z);
+	//Set the elements of a vector
+	void Set(double x,double y,double z);
+	//Get a given element of a vector
+	double& operator[](int element);
 
-  double x();
-  double y();
-  double z();
-    
-  double Quadrant();
+	//Return x,y and z coordinates of the vector
+	double x();
+	double y();
+	double z();
+
+
+	//Print out a CVector
+	void print();
+	//Get the nr of dimensions of a vector
+	int getDimensions();
+
+
+	//Add to vectors together
+	CVector operator+(const CVector& other) const;
+	//Add a number to a vector
+	CVector Add(double number) const;
+
+	//Subtract two vectors
+	CVector operator-(const CVector& other) const;
+	//Subtract a number from a vector
+	CVector Subtract(double number) const;
+
+	//Divide a vector by a number
+	CVector operator/(double number) const;
+	//Multiply a vector by a number
+	CVector operator*(double number) const;
+
+
+
+
+	//Calculate the length of a vector
+	double Length() const;
+	//Calculate the length^2 of a vector
+	double Length2() const;
+
+	//Returns a normalized vector
+	CVector Normalize() const;
+
+	//Calculate the cross product of two vectors
+	CVector Cross(const CVector& other) const;
+	//Calculate the dot product of two vectors
+	double Dot(const CVector& other) const;
+
+
+	//Calculate the sum of a vector
+	double Sum() const;
+
+	//returns the quadrant the vector is in
+	double Quadrant();
 
  protected:
-  //double x,y,z;
-  vector<double> Vector;
-  //double* Vector;
-  int Dimensions; 
+	int Dimensions;
+	vector<double> Vector;
+
 
 };
