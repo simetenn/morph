393b5dea339213cbd8a55823a97fdf142c3a471a
simetenn <simetenn@a0903317-16f6-7f7a-809d-c0b8deb45b1e>
2013-01-25 09:47:55 +0000

    Tried to reduce memory leaks

--- source/CMain.cpp
+++ source/CMain.cpp
@@ -18,7 +18,14 @@ void CMain::master() {
 	//n.CalculateAllStatistics();
 	n.HaloSort();
 	n.CalculatePhiSpherical();
-	n[10]->savePhi(myConstants::constants.outPhi);
+	n[0]->savePhi(myConstants::constants.outPhi);
+	n[2]->savePhi("Phi2.dat");
+	n[8]->savePhi("Phi3.dat");
+	n[16]->savePhi("Phi4.dat");
+	n[32]->savePhi("Phi5.dat");
+	n[64]->savePhi("Phi6.dat");	
+	n[128]->savePhi("Phi7.dat");
+	
 	n.saveSize(myConstants::constants.outSize);
 	//n.saveP(myConstants::constants.outPosition);
 }
