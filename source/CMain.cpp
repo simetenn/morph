7061829fa358c22dd02808baf1ff5e678ecc242a
simetenn <simetenn@a0903317-16f6-7f7a-809d-c0b8deb45b1e>
2013-03-29 08:29:50 +0000

    commented out mergeStatistical()

--- source/CMain.cpp
+++ source/CMain.cpp
@@ -7,9 +7,9 @@ void CMain::master() {
 	CHalos m;
 	CHalos* n;
 	//CHalo h;
-	m.loadClaudio(myConstants::constants.inFile);
+	//m.loadClaudio(myConstants::constants.inFile);
 	//m.loadData(myConstants::constants.DataFile);
-	//m.loadData("data.dat");
+	m.loadData("data.dat");
 	//m.loadHalos("splitHalos.dat");
 
 	//h.loadStructure("structureBig.dat");
@@ -20,7 +20,7 @@ void CMain::master() {
 	
 	
 	//cout << m.getNrParticles() << endl;
-	m.FriendOfFriendGrid();
+	//m.FriendOfFriendGrid();
 	//m.saveP("positionsFOF.dat");
 	//m.save(myConstants::constants.DataFile,myConstants::constants.NrParticles2File);
 	//m.FriendOfFriendN2();
@@ -52,7 +52,7 @@ void CMain::master() {
 	n[64]->savePhi("Phi6.dat");	
 	n[128]->savePhi("Phi7.dat");*/
 	
-	n->saveSize(myConstants::constants.outSize);
+	//n->saveSize(myConstants::constants.outSize);
 	//n.saveP(myConstants::constants.outPosition);
 	//n->kill();
 	delete n;
