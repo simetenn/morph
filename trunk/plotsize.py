a5a196a85614e263c446d1a92c3b1c6c07d2bedc
simetenn <simetenn@a0903317-16f6-7f7a-809d-c0b8deb45b1e>
2013-05-09 14:37:25 +0000

    added plotsize.py

---
+++ trunk/plotsize.py
@@ -0,0 +1,19 @@
+import pylab as p
+
+path = "/home/simen/Master/mybody-mpi/data/"
+filenameMine = "size.dat"
+#Read data from files
+mineData = p.loadtxt(path + filenameMine)
+
+sortedData = p.sort(mineData)[::-1]
+
+
+#print sortedData
+#p.hist(sortedDataR,bins=100)
+#p.hist(sortedData,bins=100)
+p.loglog(sortedData,range(1,len(sortedData)+1))
+#p.axis([0,9000,0,500])
+#p.legend(("mine","Rockstar","AMIGA"))
+##p.hist(p.log(rockstarData[:,1]),bins=100)
+#p.hist(mineData,bins=100)
+p.show()
