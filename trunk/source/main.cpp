bba377bf3657c5f451c823b1c6705c10e418e0d2
simetenn <simetenn@a0903317-16f6-7f7a-809d-c0b8deb45b1e>
2014-03-28 11:24:13 +0000

    Before cleaning up code

--- trunk/source/main.cpp
+++ trunk/source/main.cpp
@@ -1,14 +1,10 @@
 #include "CMain.h"
 
-//test
-
 using namespace std;
 
 
 int main(int argc, char **argv){
 	int rank,size;
-	//CMPI a;
-
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -17,7 +13,6 @@ int main(int argc, char **argv){
 	if (rank == 0) {
 		CMain m;
 		m.master();
-		
 		
 		MPI_Barrier(MPI_COMM_WORLD);
 		MPI_Finalize();
